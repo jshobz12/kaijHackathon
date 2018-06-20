4c2c5cd4553a36561b2602a9a78d84fd8cdf2beb
Ilan <ilantb@gmail.com>
2018-06-20 23:38:33 +0300

    added python file for leaderboard

---
+++ hackathon/insta.py
@@ -0,0 +1,68 @@
+from bottle import route, run, template, static_file, get, post, delete, request
+from sys import argv
+import json
+import pymysql
+
+
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='!Newyork17',
+                             db='insta',
+                             charset='utf8',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+
+@get("/scores")
+def category() :
+    try :
+        with connection.cursor() as cursor:
+            sql = """select username, handle, cast(sum(e.points) as SIGNED) as totalpts
+                    from profileInformation p
+                    join entries e on e.userid = p.id
+                    group by p.id
+                    order by totalpts desc
+                    limit 20;"""
+            cursor.execute(sql)
+            get_score = cursor.fetchall()
+            result = {
+                "STATUS" : "SUCCESS",
+                "MSG" : "success",
+                "scores" : get_score,
+                "CODE" : 200
+            }
+    except :
+        result={
+            "STATUS" : "ERROR",
+            "MSG" : "internal error",
+            "CODE" : 500
+        }
+    return json.dumps(result)
+
+
+@get("/")
+def index():
+    return template("leaderboard.html")
+
+
+@get('/js/<filename:re:.*\.js>')
+def javascripts(filename):
+    return static_file(filename, root='js')
+
+@get('/plugins/<filename:re:.*\.js>')
+def javascripts(filename):
+    return static_file(filename, root='plugins')
+
+@get('/css/<filename:re:.*\.css>')
+def stylesheets(filename):
+    return static_file(filename, root='css')
+
+@get('/plugins/<filename:re:.*\.css>')
+def stylesheets(filename):
+    return static_file(filename, root='plugins')
+
+@get('/images/<filename:re:.*\.(jpg|png|gif|ico)>')
+def images(filename):
+    return static_file(filename, root='images')
+
+
+run(host='localhost', port=7000)
---
+++ hackathon/js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $.get("/scores", function (data) {
+        response = JSON.parse(data);
+        var tab = $("<tr></tr>");
+        var ranking = $("<th></th>");
+        var username = $("<th></th>");
+        var points = $("<th></th>");
+        var instagram = $("<th></th>");
+        ranking.text("Ranking");
+        username.text("Username");
+        points.text("Points");
+        instagram.text("Instagram");
+        tab.append(ranking);
+        tab.append(username);
+        tab.append(points);
+        tab.append(instagram);
+        $("#myTable").append(tab);
+        for (var i = 0; i < response.scores.length; i++) {
+            var newPlayer = $("<tr></tr>");
+            var playerRanking = $("<th></th>");
+            var playerName = $("<th></th>");
+            var playerPoints = $("<th></th>");
+            var playerProfile = $("<th></th>");
+            playerRanking.text(i+1);
+            playerName.text(response.scores[i].username);
+            playerPoints.text(response.scores[i].totalpts);
+            playerProfile.text(response.scores[i].handle);
+            newPlayer.append(playerRanking);
+            newPlayer.append(playerName);
+            newPlayer.append(playerPoints);
+            newPlayer.append(playerProfile);
+            $("#myTable").append(newPlayer);
+        }
+    });
+
+});
+
