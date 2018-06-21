50d9f22ae5c802923ff56c46c2d7bce4918a19a4
Ilan <ilantb@gmail.com>
2018-06-21 13:48:00 +0300

    photos in the index

--- hackathon/js/homePhotos.js
+++ hackathon/js/homePhotos.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $.get("/all_photos", function (data) {
         response = JSON.parse(data)
+        console.log(response);
         for (var i = 0; i < response.photos.length; i++) {
             var newImage = $("<img>");
             newImage.attr("src",response.photos[i].imgURL);
