0bdeb64097b6504ee1cdbfada4aecfe114127e4b
Ariel Huntley <arihuntley@gmail.com>
2018-06-21 13:10:03 +0300

    get everything working together

--- hackathon/consts.py
+++ hackathon/consts.py
@@ -3,8 +3,11 @@ import os
 TAGS = ['healthylivingtlvchallenge', ]
 
 
-ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-assert ACCESS_TOKEN
-DB_ADDRESS = 'localhost'
-DB_USER = 'arielhuntley'
-DB = 'INSTA'
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+
+DB_PARAMS = dict(host='localhost',
+                     user='arielhuntley',
+                     db='instadb',
+                     charset='utf8',)
+
+DB_PARAMS = dict(**DB_PARAMS, user='root', password='!Newyork17')
