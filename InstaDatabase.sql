4bac3856c03d178638f3de7815c6344fb5237ef9
Kira <kirailexaweiss@gmail.com>
2018-06-21 10:18:38 +0300

    m

--- InstaDatabase.sql
+++ InstaDatabase.sql
@@ -29,7 +29,7 @@ Describe entries;
 
 
 INSERT INTO profileInformation (username,email,password,handle,totalPoints)
-VALUES("kweiss20","kiraweiss@me.com","dog1234","@kiraweiss",0);
+VALUES("kweiss20","kiraweiss@me.com","dog1234","@kiraweiss",23);
 
 INSERT INTO entries (userId,dateAndTime,insta,instaPostId,points)
 VALUES(1,'2018-06-19 12:34:22',"linkToInsta.com",1111,2);
@@ -40,7 +40,7 @@ VALUES(1,'2018-06-20 15:07:20',"linkToInsta.com",1112,2);
 
 
 INSERT INTO profileInformation (username,email,password,handle,totalPoints)
-VALUES("JoeSmith","JoeSmith@gmail.com","rockon665","@joe_the_rock",0);
+VALUES("JoeSmith","JoeSmith@gmail.com","rockon665","@joe_the_rock",54);
 
 INSERT INTO entries (userId,dateAndTime,insta,instaPostId,points)
 VALUES(2,'2018-06-16 13:55:01',"linkToInsta.com",1113,2);
@@ -54,7 +54,7 @@ VALUES(2,'2018-06-19 19:14:48',"linkToInsta.com",1115,2);
 
 
 INSERT INTO profileInformation (username,email,password,handle,totalPoints)
-VALUES("fitgirl22","sarah_fitness@hotmail.com","smoothiesonly0","@fitgirl220",0);
+VALUES("fitgirl22","sarah_fitness@hotmail.com","smoothiesonly0","@fitgirl220",56);
 
 INSERT INTO entries (userId,dateAndTime,insta,instaPostId,points)
 VALUES(3,'2018-06-20 8:31:03',"linkToInsta.com",1116,2);
